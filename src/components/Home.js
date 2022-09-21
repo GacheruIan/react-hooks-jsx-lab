09b281f4699be58d7e5fe8181b4bf0b59760c115
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-09-21 10:22:41 +0300

    Done with assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  const styleObj = {
+    color : "firebrick"
+  }
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">Home
+    <h1 style={styleObj}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
